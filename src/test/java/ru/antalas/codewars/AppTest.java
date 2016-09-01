2a5717e700d2a9e8944f263d4834002f4df04b52
salatna <anton.shkrunin@gmail.com>
2016-09-01 01:42:00 +0100

    First commit

---
+++ src/test/java/ru/antalas/codewars/AppTest.java
@@ -0,0 +1,11 @@
+package ru.antalas.codewars;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void shouldPass() throws Exception {
+        Assert.assertTrue(true);
+    }
+}
